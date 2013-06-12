e61b15bbf88ffc9d408cd9bd5338ed1f6de16bd1
marcosalis <marco@onlulu.com>
2013-06-12 19:00:30 +0100

    ParallelAsyncTasksManager: improved documentation

--- src/com/github/luluvise/droid_utils/tasks/ParallelAsyncTasksManager.java
+++ src/com/github/luluvise/droid_utils/tasks/ParallelAsyncTasksManager.java
@@ -7,47 +7,77 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.NotThreadSafe;
 
+import android.os.AsyncTask;
+
 import com.google.common.annotations.Beta;
 
 /**
- * Small component that allows managing the lifecycle (execute, cancel) of
- * multiple {@link ParallelAsyncTask}s at the same time.
+ * Small component that allows managing the lifecycle (execution and
+ * cancelation) of multiple {@link ParallelAsyncTask}s at the same time.
  * 
  * @since 1.5
  * @author Marco Salis
- * 
- * @param <Params>
- *            The Param type of the tasks to add
  */
 @Beta
 @NotThreadSafe
-@SuppressWarnings("rawtypes")
-public class ParallelAsyncTasksManager<T extends ParallelAsyncTask> {
+public class ParallelAsyncTasksManager {
 
 	@Nonnull
-	private final ArrayList<T> mManagedTasks;
+	private final ArrayList<ParallelAsyncTask<?, ?, ?>> mManagedTasks;
 
+	/**
+	 * Constructor for a {@link ParallelAsyncTasksManager}
+	 */
 	public ParallelAsyncTasksManager() {
-		mManagedTasks = new ArrayList<T>();
+		mManagedTasks = new ArrayList<ParallelAsyncTask<?, ?, ?>>();
 	}
 
-	public void addTask(@Nonnull T task) {
+	/**
+	 * Adds a {@link ParallelAsyncTask} to the manager without executing it
+	 * 
+	 * @param task
+	 *            The task to add to the manager
+	 */
+	public void addTask(@Nonnull ParallelAsyncTask<?, ?, ?> task) {
 		mManagedTasks.add(task);
 	}
 
-	public void addAllTasks(@Nonnull Collection<? extends T> tasks) {
+	/**
+	 * Adds a {@link ParallelAsyncTask} to the manager and executes it.
+	 * 
+	 * @param task
+	 *            The task to add to the manager and execute
+	 * @param params
+	 *            The params to pass to
+	 *            {@link ParallelAsyncTask#parallelExec(Object...)}
+	 * @return The executed {@link ParallelAsyncTask}
+	 */
+	@Nonnull
+	public <Params> ParallelAsyncTask<Params, ?, ?> addAndExecute(
+			@Nonnull ParallelAsyncTask<Params, ?, ?> task, @Nullable Params... params) {
+		addTask(task);
+		return (ParallelAsyncTask<Params, ?, ?>) task.parallelExec(params);
+	}
+
+	/**
+	 * Adds a collection of {@link ParallelAsyncTask} to the manager without
+	 * executing them.
+	 * 
+	 * @param tasks
+	 *            A {@link Collection} of async tasks
+	 */
+	public void addAllTasks(@Nonnull Collection<? extends ParallelAsyncTask<?, ?, ?>> tasks) {
 		mManagedTasks.addAll(tasks);
 	}
 
-	@Nonnull
-	@SuppressWarnings("unchecked")
-	public <Params> T addAndExecute(@Nonnull T task, @Nullable Params... params) {
-		addTask(task);
-		return (T) task.parallelExec(params);
-	}
-
+	/**
+	 * Cancels and removes all async tasks previously added to the manager.
+	 * 
+	 * @param mayInterruptIfRunning
+	 *            See {@link AsyncTask#cancel(boolean)}
+	 */
 	public void cancelAllTasks(boolean mayInterruptIfRunning) {
-		for (T task : mManagedTasks) {
+		for (ParallelAsyncTask<?, ?, ?> task : mManagedTasks) {
 			if (task != null) {
 				task.cancel(mayInterruptIfRunning);
 			}
